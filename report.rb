4e519a938cb2f9c5185b138d72c4a11fed074247
Benji Wiebe <benjiwiebe14@gmail.com>
2022-02-02 15:20:30 -0600

    Ignore spring order

--- report.rb
+++ report.rb
@@ -2,6 +2,9 @@
 require 'json'
 require 'pry'
 
+spring_order = 'SO154761'
+ignore_spring_order = true
+
 bars = /^\d{4} \d{4}$/
 units = /^[A-Z]{1,3} \d{2,3}/
 
@@ -11,6 +14,10 @@ obj = JSON.parse(File.read("sesca_out.json"), {symbolize_names: true})
 unshipped_items = {}
 
 obj.each do |order|
+	if order[:order] == spring_order && ignore_spring_order
+		puts "Ignoring spring order ##{spring_order}"
+		next
+	end
 	order[:sections].each do |section|
 		next if section[:shipped]
 		items = section[:items]
